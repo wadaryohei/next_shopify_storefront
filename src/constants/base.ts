80ff2fa4e50d6dd928870a70a3cd39d9804d5ef9
wadaryohei <ryohei.w0317wr@icloud.com>
2023-01-09 00:47:51 +0900

    #1 定数を追加

--- src/constants/base.ts
+++ src/constants/base.ts
@@ -1,2 +1,4 @@
 export const SITE_TITLE = 'YURAGI Ambient Room' as const;
 export const SITE_DESCRIPTION = '聴覚と視覚で、五感すべてで揺らぐ時間を楽しもう。' as const;
+export const SITE_IMAGE = 'og/default.png' as const;
+export const SITE_URL = 'https://yuragi-ambient-room.vercel.app' as const;
